bdc30c173e287964a4936253ed4f42f77a8f6b11
1SkYVValkeR1 <96080349+1SkYVValkeR1@users.noreply.github.com>
2022-03-11 15:48:05 +0100

    Add files via upload
    Domashni C3 Oop

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeWork.Class02.Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SwapNumbers");
+
+            int firstNumber = 8;
+            int secondNumber = 5;
+            int temp;
+
+            Console.WriteLine("please enter the firstNumber");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter the secondNumber");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            temp = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = temp;
+
+            Console.WriteLine("Values are swapping are:" );
+            Console.WriteLine("firstNumber="+firstNumber);
+            Console.WriteLine("secondNumber="+secondNumber);
+            
+        }
+    }
+}
