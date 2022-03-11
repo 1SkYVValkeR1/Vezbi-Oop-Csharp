8eb9051453ef68182ec61520a0aa58417fcda9b9
1SkYVValkeR1 <96080349+1SkYVValkeR1@users.noreply.github.com>
2022-03-11 15:49:16 +0100

    Exercise 02
    Exercise 02 C#

--- Program.cs
+++ Program.cs
@@ -1,31 +1,20 @@
 ﻿using System;
 
-namespace HomeWork.Class02.Exercise03
+namespace HomeWork.Class02.Exercise01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("SwapNumbers");
+            Console.WriteLine("RealCalculator");
 
-            int firstNumber = 8;
-            int secondNumber = 5;
-            int temp;
+            int firstNumber = 10;
+            int secondNumber = 15;
 
-            Console.WriteLine("please enter the firstNumber");
-            firstNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("please enter the secondNumber");
-            secondNumber = Convert.ToInt32(Console.ReadLine());
+            int result = firstNumber + secondNumber;
 
+            Console.WriteLine("theresult is:" +result);
 
-            temp = firstNumber;
-            firstNumber = secondNumber;
-            secondNumber = temp;
-
-            Console.WriteLine("Values are swapping are:" );
-            Console.WriteLine("firstNumber="+firstNumber);
-            Console.WriteLine("secondNumber="+secondNumber);
-            
         }
     }
 }
